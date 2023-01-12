6932c9b7c1d7f99e3d9749a3b14a6615c522f07f
huang-zong <2623628820@qq.com>
2023-01-12 23:55:36 +0800

    这是简单的输出

---
+++ simple output/simple output/simple output.c
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+////输出hello nowcoder!
+//int main()
+//{
+//	//输出
+//	printf("Hello Nowcoder!");
+//	return 0;
+//}
+
+
+//输出BC2小飞机
+int main()
+{
+	//输出
+	printf("     **     \n");
+	printf("     **     \n");
+	printf("************\n");
+	printf("************\n");
+	printf("    *  *    \n");
+	printf("    *  *    \n");
+	return 0;
+
+}
